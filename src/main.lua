f13e27a3fe824c76dadabcd0c4db3523c9e05955
tim2cc <ahtyamov.tam@gmail.com>
2023-09-17 17:08:56 +0300

    player movenment and score upd

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,6 @@
+-- love 2d engine https://love2d.org/wiki/love
+
+-- https://github.com/Ulydev/push - lib
 push = require "push"
 
 WIDTH = 1200
@@ -9,16 +12,18 @@ VIRTUAL_HEIGHT = 243
 PADDLE_SPEED = 200
 
 function love.load()
-
+    -- players variables
     player1Score = 0
     player2Score = 0
-    paddle1X = VIRTUAL_WIDTH - 10   
+    paddle1X = 10   
     paddle1Y = VIRTUAL_HEIGHT - 50
+    paddle2X = VIRTUAL_WIDTH - 14
+    paddle2Y = 50
     
 
-
+    -- graphics rendering
     love.graphics.setDefaultFilter('nearest', "nearest")
-
+    -- font setup
     smallFont = love.graphics.newFont('/resources/prstartk.ttf', 8)
 
     scoreFont = love.graphics.newFont('/resources/prstartk.ttf', 24)
@@ -29,6 +34,22 @@ function love.load()
         resizable = false,
         vsync = true
     })   
+end
+
+function love.update(dt)
+    -- paddle 1 movenment
+    if love.keyboard.isDown('a') then
+        paddle1Y = paddle1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('z') then
+        paddle1Y = paddle1Y + PADDLE_SPEED * dt
+    end
+    
+    -- paddle 2 movenment
+    if love.keyboard.isDown('k') then
+        paddle2Y = paddle2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('m') then
+        paddle2Y = paddle2Y + PADDLE_SPEED * dt
+    end
 end
 
 
@@ -52,8 +73,8 @@ function love.draw()
 
     )
 
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', paddle2X, paddle2Y, 5, 20)
     love.graphics.rectangle('fill',paddle1X, paddle1Y, 5, 20)
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH / 2 - 2 , VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     push:apply('end') 
 end
